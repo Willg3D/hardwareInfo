d2366b7e0fa5bc7ecefad51959fa3c98970a36ef
William Glass <willg.glass94@gmail.com>
2023-10-05 15:23:57 -0800

    simplified terminal display

--- display.py
+++ display.py
@@ -1,44 +1,22 @@
-import curses
+def terminalDisplay(displayStrings = None):
+    if not displayStrings:
+        return
 
-testTextArray = ["  System Hardware Information     ",
-                  "     CPU Model: Intel Something", "     CPU Core: 1000"]
+    maxLength = max(len(i) for i in displayStrings)
+    boxWidth = maxLength + 2
+    rowLine = '═' * boxWidth
 
-def terminalDisplay(stdscr, displayStrings=None):
-    # Initialize curses
-    curses.curs_set(0)
-    stdscr.clear()
-    stdscr.refresh()
-
-    # Calculate box dimensions
-    maxY, maxX = stdscr.getmaxyx()
-    boxHeight = maxY - 2
-    boxWidth = maxX - 2
-
-    # Create a box
-    box = curses.newwin(boxHeight, boxWidth, 1, 1)
-    box.box()
-
-    if displayStrings is not None:
-        # Display the provided strings inside the box
-        for i, string in enumerate(displayStrings):
-            box.addstr(i + 1, 1, string)
-
-    box.refresh()
-
-    # Wait for user to press Enter
-    stdscr.addstr(maxY - 1, 0, "Press Enter to exit...")
-    stdscr.refresh()
-
-    # Wait for the Enter key to be pressed
-    while True:
-        key = stdscr.getch()
-        if key == ord('\n'):  # Check for Enter key press
-            break
-
-
+    print('╔' + rowLine + '╗')
+    for string in displayStrings:
+        formattedString = f'║ {string:{maxLength}} ║'
+        print(formattedString)
+    print('╚' + rowLine + '╝')
 
 """
-# Run the terminalDisplay function
-curses.wrapper(terminalDisplay, testTextArray)
+
+testTextArray = ["System Hardware Information",
+                  "CPU Model: Intel Something", "CPU Core: 1000"]
+
+terminalDisplay(testTextArray)
 
 """
