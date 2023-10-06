3c81e31ebda38e7f1151db56cd7f7eaee6a14337
raheem199 <143669458+raheem199@users.noreply.github.com>
2023-10-06 10:38:29 -0800

    Add files via upload

---
+++ CPU_info.py
@@ -0,0 +1,27 @@
+import psutil
+import subprocess
+
+# Number of logical and physical cores
+print(f"Logical cores: {psutil.cpu_count(logical=True)}")
+print(f"Physical cores: {psutil.cpu_count(logical=False)}")
+
+# CPU frequencies
+#cpu_freq = psutil.cpu_freq()
+#print(f"Current frequency: {cpu_freq.current}Mhz")
+#print(f"Max frequency: {cpu_freq.max}Mhz")
+#print(f"Min frequency: {cpu_freq.min}Mhz")
+def get_cpu_freq():
+    result = subprocess.check_output(["sysctl", "-n", "machdep.cpu.brand_string"]).decode('utf-8')
+    return result.strip()
+
+print(f"CPU Info: {get_cpu_freq()}")
+
+# CPU percent usage
+print(f"CPU usage (per core): {psutil.cpu_percent(interval=1, percpu=True)}%")
+print(f"Total CPU usage: {psutil.cpu_percent(interval=1)}%")
+
+# CPU times
+cpu_times = psutil.cpu_times()
+print(f"User time: {cpu_times.user}s")
+print(f"System time: {cpu_times.system}s")
+print(f"Idle time: {cpu_times.idle}s")
