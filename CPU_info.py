306dc1a0ca0b4bcd930db4ce9fc4a40a49fb1ebf
bfforsythe <bfforsythe@alaska.edu>
2023-10-09 18:28:52 -0800

    repush

--- CPU_info.py
+++ CPU_info.py
@@ -10,26 +10,38 @@ print(f"Physical cores: {psutil.cpu_count(logical=False)}")
 
 
 # CPU frequencies (Windows / Linux)
-if hardware == "Windows" or hardware == "Linux":
+def win_cpu_freq(): 
     cpu_freq = psutil.cpu_freq()
-    print(f"Current frequency: {cpu_freq.current}Mhz")
-    print(f"Max frequency: {cpu_freq.max}Mhz")
-    print(f"Min frequency: {cpu_freq.min}Mhz")
-
-# CPU frequencies (Mac)
-else:
-    def get_cpu_freq():
-        result = subprocess.check_output(["sysctl", "-n", "machdep.cpu.brand_string"]).decode('utf-8')
-        return result.strip()
-
-        print(f"CPU Info: {get_cpu_freq()}")
-
+    cpu_times = psutil.cpu_times()
+    info = (
+        f"Current frequency: {cpu_freq.current}Mhz\n"
+        f"Max Frequency: {cpu_freq.max}Mhz\n"
+        f"Min Frequency: {cpu_freq.min}Mhz\n"
         # CPU percent usage
-        print(f"CPU usage (per core): {psutil.cpu_percent(interval=1, percpu=True)}%")
-        print(f"Total CPU usage: {psutil.cpu_percent(interval=1)}%")
+        f"CPU usage (per core): {psutil.cpu_percent(interval=1, percpu=True)}%\n"
+        f"Total CPU usage: {psutil.cpu_percent(interval=1)}%\n"
 
         # CPU times
-        cpu_times = psutil.cpu_times()
-        print(f"User time: {cpu_times.user}s")
-        print(f"System time: {cpu_times.system}s")
-        print(f"Idle time: {cpu_times.idle}s")
+        
+        f"User time: {cpu_times.user}s\n"
+        f"System time: {cpu_times.system}s\n"
+        f"Idle time: {cpu_times.idle}s\n"
+    )
+    return info
+
+# CPU frequencies (Mac)
+def get_cpu_freq():
+    result = subprocess.check_output(["sysctl", "-n", "machdep.cpu.brand_string"]).decode('utf-8')
+    return result.strip()
+
+    print(f"CPU Info: {get_cpu_freq()}")
+
+# CPU percent usage
+print(f"CPU usage (per core): {psutil.cpu_percent(interval=1, percpu=True)}%")
+print(f"Total CPU usage: {psutil.cpu_percent(interval=1)}%")
+
+# CPU times
+cpu_times = psutil.cpu_times()
+print(f"User time: {cpu_times.user}s")
+print(f"System time: {cpu_times.system}s")
+print(f"Idle time: {cpu_times.idle}s")
