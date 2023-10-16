6d8d093e1fff7743ac0273b146cdb05645c6ec25
William Glass <willg.glass94@gmail.com>
2023-10-15 21:28:08 -0800

    gpu_info now works for nvidia

--- GPU_info.py
+++ GPU_info.py
@@ -12,6 +12,6 @@ for gpu in gpus:
     print(f"GPU Memory Used: {gpu.memoryUsed}MB")
     print(f"GPU Memory Total: {gpu.memoryTotal}MB")
     print(f"GPU Memory Utilization: {gpu.memoryUtil * 100:.1f}%")
-    print(f"GPU Core Clock: {gpu.gpuClock}MHz")
-    print(f"GPU Memory Clock: {gpu.memoryClock}MHz")
+    #print(f"GPU Core Clock: {gpu.gpuClock}MHz")
+    #print(f"GPU Memory Clock: {gpu.memoryClock}MHz")
     print("----------------------------------------------------")
